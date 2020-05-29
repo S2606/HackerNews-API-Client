0ecbdb7c0811a75ac60a13e1ede189bd0bcbb876
Shagun Khemka <shagun.khemka60@gmail.com>
2020-05-29 19:14:06 +0530

    Remove irrelevant dependencies

--- hackernews-api/src/main/java/com/paytm/insider/api/HackernewsApiApplication.java
+++ hackernews-api/src/main/java/com/paytm/insider/api/HackernewsApiApplication.java
@@ -4,8 +4,6 @@ import com.paytm.insider.service.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Import(ServiceConfiguration.class)
